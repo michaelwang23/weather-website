9c62979e370e7d7683f885dbc35b30d170f2a74c
Michael Wang <michaelwang@Michaels-MacBook-Pro.local>
2019-08-04 16:35:46 -0700

    Fix

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out with a high of ' + body.daily.data[0].apparentTemperatureHigh + 'degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out with a high of ' + body.daily.data[0].apparentTemperatureHigh + ' degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
