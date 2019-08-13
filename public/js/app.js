7749b76e3246917722bcf029ae477f2dc2f35355
Michael Wang <michaelwang@Michaels-MacBook-Pro.local>
2019-08-13 01:05:57 -0700

    Pic changes

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
+const pictureOne = document.querySelector('#image-1')
 //messageOne.textContent = "From Javascript"
 
 weatherForm.addEventListener("submit", (event) => {
@@ -11,6 +11,7 @@ weatherForm.addEventListener("submit", (event) => {
 
   messageOne.textContent = "Loading"
   messageTwo.textContent = ""
+
 
   fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
@@ -26,5 +27,16 @@ weatherForm.addEventListener("submit", (event) => {
       }
     })
   })
+
+  var client_id = "e72d3972ba3ff93da57a4c0be4f0b7323346c136b73794e2a01226216076655b"
+  var query = location
+  var url = "https://api.unsplash.com/search/photos?page=1&query=" + encodeURIComponent(query) + "&client_id=" + client_id
+
+  fetch(url)
+    .then(res => res.json())
+    .then(json => pictureOne.src = json.results[0].urls.thumb);
+  
+
+
 
 })
